056125e6ea06f6e32739b906ac1906d4ef14e48f
Pratik More <developer@pratikmore.com>
2023-06-28 16:44:02 -0400

    Video Added

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { BsInstagram, BsArrowLeftShort, BsArrowRight } from 'react-icons/bs';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Gallery.css';
 
 const Gallery = () => (
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+
+import { meal} from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setplayVideo] = React.useState(false)
+  const videoRef = React.useRef()
+
+  const handleVideo = () => {
+    setplayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if(playVideo){
+      videoRef.current.pause()
+
+    }else{
+      videoRef.current.play()
+    }
+  }
+  return(
+  <div className='app__video'>
+   <video src={meal}
+   ref={videoRef}
+   type="video/mp4"
+   loop
+   controls={false}
+   muted/> 
+   
+
+   <div className='app__video-overlay flex__center'>
+    <div className= "app__video-overlay_circle flex__center" onClick={handleVideo}>
+      {playVideo
+      ? <BsPauseFill color='#fff' fontSize={30}/> : <BsFillPlayFill color='#fff' fontSize={30}/>
+      }
+    </div>
+   </div>
   </div>
-);
+  )
+};
 
 export default Intro;
