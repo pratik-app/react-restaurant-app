50827a4cadfcf51181e6e7efd1c6be147f62d878
Pratik More <developer@pratikmore.com>
2023-06-27 23:47:16 -0400

    Special Menu is Completed

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -2,9 +2,48 @@ import React from 'react';
 
 import './SpecialMenu.css';
 
+
+import { SubHeading, MenuItem } from '../../components';
+import {images, data } from '../../constants'
+
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+        <SubHeading title="Menu that fits you palatte"/>
+
+        <h1 className='headtext__cormorant'>Today's Special</h1>
+
+    </div>
+
+
+    <div className='app__specialMenu-menu'>
+
+      <div className='app__specialMenu-menu_wine flex__center'>
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+            {data.wines.map(( wine, index ) => (
+              <MenuItem key={wine.title + index}  title={wine.title} price={wine.price} tags={wine.tags}/>
+            ))}
+        </div>
+      </div>
+
+      <div className='app__specialMenu-menu_img'>
+          <img src={images.menu} alt='menu Image'/>
+      </div>
+      
+      <div className='app__specialMenu-menu_cocktail flex__center'>
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+            {data.cocktails.map(( cocktails, index ) => (
+              <MenuItem key={cocktails.title + index} title={cocktails.title} price={cocktails.price} tags={cocktails.tags}/>
+            ))}
+        </div>
+      </div>
+    </div>
+
+    <div style={{margin: '15px' }}>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
   </div>
 );
 
